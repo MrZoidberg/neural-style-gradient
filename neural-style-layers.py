e3aacd07d7ef048fc5be96edcf0b0eeed4622d2b
Mikhail Merkulov <mihail.merkulov@gmail.com>
2016-12-19 20:44:27 +0200

    layers script

--- neural-style-layers.py
+++ neural-style-layers.py
@@ -1,6 +1,40 @@
-
-import sys
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import sys, shlex, subprocess, os
+from itertools import combinations, chain
 from argparse import ArgumentParser
+from os.path import expanduser, join, splitext, normpath, basename
+
+############################
+#image size, optimizer, save/print frequency & quality adam
+#or lbfgs, adam is faster, lbfgs is higher quality
+PRINT_ITER = 50
+ITER_COUNT = 150
+SAVE_ITER = ITER_COUNT
+IMAGE_SIZE = 200
+SEED = 123
+
+TV_WEIGHT = 0.0002
+CONTENT_WEIGHT = 9
+STYLE_WEIGHT = 200
+STYLE_SCALE = 1.3
+INIT_SOURCE = "image"
+
+GPU = 0
+OPTIMIZER = "adam"
+BACKEND = "cudnn"
+
+NEURAL_STYLE_PATH = "Documents/code/zoid-neural-style/"
+RUN_SCRIPT_NAME = "neural_style.lua"
+MODEL_FILE = "models/VGG_ILSVRC_19_layers.caffemodel"
+PROTO_FILE = "models/VGG_ILSVRC_19_layers_deploy.prototxt"
+LAYERS_CONTENT = ["relu4_2"]
+LAYERS_STYLE = ["relu1_1", "relu1_2",
+                "relu2_1", "relu2_2",
+                "relu3_1", "relu3_2", "relu3_3", "relu3_4",
+                "relu4_1", "relu4_3", "relu4_4",
+                "relu5_1", "relu5_2", "relu5_3", "relu5_4"]
 
 
 def build_parser():
@@ -15,19 +49,89 @@ def build_parser():
                         metavar='IN_PATH', required=True)
 
     parser.add_argument('--out-path', type=str,
-                        dest='out_path', help=help_out, metavar='OUT_PATH',
+                        dest='out_path', help="output directory", metavar='OUT_PATH',
                         required=True)
 
-    parser.add_argument('--device', type=str,
-                        dest='device',help='device to perform compute on',
-                        metavar='DEVICE', default=DEVICE)
-
-    parser.add_argument('--batch-size', type=int,
-                        dest='batch_size',help='batch size for feedforwarding',
-                        metavar='BATCH_SIZE', default=BATCH_SIZE)
-
-    parser.add_argument('--allow-different-dimensions', action='store_true',
-                        dest='allow_different_dimensions',
-                        help='allow different image dimensions')
-
     return parser
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    lst = list(iterable)
+    return chain.from_iterable(combinations(lst, r) for r in range(len(lst)+1))
+
+def main():
+    parser = build_parser()
+    options = parser.parse_args()
+    '''
+    TBD th neural_style.lua \
+ 1 -init image \
+-style_image ../in/pic/src.jpg -content_image ../in/pic/dst4.jpg \
+-output_image ../in/pic/outdst4d.jpg \
+-image_size 1000 -content_weight 0.000001 -style_weight 100000 \
+-save_iter 50 -num_iterations 1000 \
+-model_file models/nin_imagenet_conv.caffemodel \
+-proto_file models/train_val.prototxt \
+-content_layers relu0,relu1,relu2,relu3,relu5,relu6,relu7,relu8,relu9,relu10 \
+-style_layers relu0,relu1,relu2,relu3,relu5,relu6,relu7,relu8,relu9,relu10
+    '''
+    home = expanduser("~")
+    print('The home directory is {0}'.format(home))
+
+    output_dir = expanduser(join(options.out_path, splitext(basename(options.input_style))[0], splitext(basename(options.in_file))[0]))
+    print('The output directory is {0}'.format(output_dir))
+
+    scriptPath = normpath(join(home, NEURAL_STYLE_PATH, RUN_SCRIPT_NAME))
+    print('The script path is {0}'.format(scriptPath))
+
+    styleFile = expanduser(options.input_style)
+    print('The style path is {0}'.format(styleFile))
+
+    inputFile = expanduser(options.in_file)
+    print('The style path is {0}'.format(inputFile))
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    styles = list(powerset(LAYERS_STYLE))
+    print('Using {0} pairs of styles'.format(len(styles)))
+
+    for style in styles:
+        if len(style) > 0:
+            print('Style layers: {0}'.format(".".join(style)))
+
+            runScript = 'th {script} -style_scale {style_scale} -init {init} -style_image "{style_image}" \
+-content_image "{content_image}" -image_size {image_size} -output_image "{output_image}" \
+-content_weight {content_weight} -style_weight {style_weight} -save_iter {save_iter} \
+-num_iterations {num_iterations} -content_layers {content_layers} -style_layers {style_layers} \
+-gpu {gpu} -optimizer {optimizer} -tv_weight {tv_weight} -backend {backend} -seed {seed}'.format(
+        script = scriptPath,
+        style_scale = STYLE_SCALE,
+        init = INIT_SOURCE,
+        style_image = styleFile,
+        content_image = inputFile,
+        image_size = IMAGE_SIZE,
+        output_image = join(output_dir, '{0}.jpg'.format(".".join(style))),
+        content_weight = CONTENT_WEIGHT,
+        style_weight = STYLE_WEIGHT,
+        save_iter = SAVE_ITER,
+        num_iterations = ITER_COUNT,
+        content_layers = ",".join(LAYERS_CONTENT),
+        style_layers = ",".join(style),
+        gpu = GPU,
+        optimizer = OPTIMIZER,
+        tv_weight = TV_WEIGHT,
+        backend = BACKEND,
+        seed = SEED)
+
+            print('Script \'{0}\''.format(runScript))
+
+            with subprocess.Popen(runScript, shell=True,
+                                  stdin=subprocess.PIPE,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE,
+                                  universal_newlines=True,
+                                  cwd=join(home, NEURAL_STYLE_PATH)) as proc:
+                print(proc.stdout.read())
+
+if __name__ == '__main__':
+    main()

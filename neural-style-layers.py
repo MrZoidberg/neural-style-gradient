69afd422e9659a6d09536d8ad26b7ba6ea014b43
Mikhail Merkulov <mikhailm@benders-Mac-mini-7.local>
2016-12-21 11:39:13 +0200

    added progress indication

--- neural-style-layers.py
+++ neural-style-layers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import sys, shlex, subprocess, os
+import sys, subprocess, os
 from itertools import combinations, chain
 from argparse import ArgumentParser
 from os.path import expanduser, join, splitext, normpath, basename
@@ -12,16 +12,16 @@ from os.path import expanduser, join, splitext, normpath, basename
 PRINT_ITER = 50
 ITER_COUNT = 150
 SAVE_ITER = ITER_COUNT
-IMAGE_SIZE = 200
+IMAGE_SIZE = 250
 SEED = 123
 
 TV_WEIGHT = 0.0002
 CONTENT_WEIGHT = 9
-STYLE_WEIGHT = 200
-STYLE_SCALE = 1.3
+STYLE_WEIGHT = 250
+STYLE_SCALE = 1
 INIT_SOURCE = "image"
 
-GPU = 0
+GPU = -1
 OPTIMIZER = "adam"
 BACKEND = "cudnn"
 
@@ -30,12 +30,18 @@ RUN_SCRIPT_NAME = "neural_style.lua"
 MODEL_FILE = "models/VGG_ILSVRC_19_layers.caffemodel"
 PROTO_FILE = "models/VGG_ILSVRC_19_layers_deploy.prototxt"
 LAYERS_CONTENT = ["relu4_2"]
+''' All styles:
 LAYERS_STYLE = ["relu1_1", "relu1_2",
                 "relu2_1", "relu2_2",
                 "relu3_1", "relu3_2", "relu3_3", "relu3_4",
                 "relu4_1", "relu4_3", "relu4_4",
                 "relu5_1", "relu5_2", "relu5_3", "relu5_4"]
-
+'''
+LAYERS_STYLE = ["relu1_1",
+                "relu2_2",
+                "relu3_3",
+                "relu4_4",
+                "relu5_4"]
 
 def build_parser():
     parser = ArgumentParser()
@@ -58,6 +64,27 @@ def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     lst = list(iterable)
     return chain.from_iterable(combinations(lst, r) for r in range(len(lst)+1))
+
+# Print iterations progress
+def printProgress(iteration, total, prefix='', suffix='', decimals=1, barLength=100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        barLength   - Optional  : character length of bar (Int)
+    """
+    formatStr = "{0:." + str(decimals) + "f}"
+    percent = formatStr.format(100 * (iteration / float(total)))
+    filledLength = int(round(barLength * iteration / float(total)))
+    bar = '█' * filledLength + '-' * (barLength - filledLength)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix)),
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
 
 def main():
     parser = build_parser()
@@ -93,9 +120,12 @@ def main():
         os.makedirs(output_dir)
 
     styles = list(powerset(LAYERS_STYLE))
-    print('Using {0} pairs of styles'.format(len(styles)))
+    stylesCount = len(styles)
+    print('Using {0} pairs of styles'.format(stylesCount))
 
+    i = -1
     for style in styles:
+        i = i + 1
         if len(style) > 0:
             print('Style layers: {0}'.format(".".join(style)))
 
@@ -103,27 +133,29 @@ def main():
 -content_image "{content_image}" -image_size {image_size} -output_image "{output_image}" \
 -content_weight {content_weight} -style_weight {style_weight} -save_iter {save_iter} \
 -num_iterations {num_iterations} -content_layers {content_layers} -style_layers {style_layers} \
--gpu {gpu} -optimizer {optimizer} -tv_weight {tv_weight} -backend {backend} -seed {seed}'.format(
-        script = scriptPath,
-        style_scale = STYLE_SCALE,
-        init = INIT_SOURCE,
-        style_image = styleFile,
-        content_image = inputFile,
-        image_size = IMAGE_SIZE,
-        output_image = join(output_dir, '{0}.jpg'.format(".".join(style))),
-        content_weight = CONTENT_WEIGHT,
-        style_weight = STYLE_WEIGHT,
-        save_iter = SAVE_ITER,
-        num_iterations = ITER_COUNT,
-        content_layers = ",".join(LAYERS_CONTENT),
-        style_layers = ",".join(style),
-        gpu = GPU,
-        optimizer = OPTIMIZER,
-        tv_weight = TV_WEIGHT,
-        backend = BACKEND,
-        seed = SEED)
+-gpu {gpu} -optimizer {optimizer} -tv_weight {tv_weight} -backend {backend} -seed {seed} -normalize_gradients'.format(
+        script=scriptPath,
+        style_scale=STYLE_SCALE,
+        init=INIT_SOURCE,
+        style_image=styleFile,
+        content_image=inputFile,
+        image_size=IMAGE_SIZE,
+        output_image=join(output_dir, '{0}.jpg'.format(".".join(style))),
+        content_weight=CONTENT_WEIGHT,
+        style_weight=STYLE_WEIGHT,
+        save_iter=SAVE_ITER,
+        num_iterations=ITER_COUNT,
+        content_layers=",".join(LAYERS_CONTENT),
+        style_layers=",".join(style),
+        gpu=GPU,
+        optimizer=OPTIMIZER,
+        tv_weight=TV_WEIGHT,
+        backend=BACKEND,
+        seed=SEED)
 
-            print('Script \'{0}\''.format(runScript))
+            #print('Script \'{0}\''.format(runScript))
+            printProgress(i, stylesCount, prefix='Progress:',
+                          suffix='Complete', barLength=100)
 
             with subprocess.Popen(runScript, shell=True,
                                   stdin=subprocess.PIPE,
